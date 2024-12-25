969e3c8a3e63ced2bad34c4de547c87f0ddd46bf
Haroon-S <harryshahid420@gmail.com>
2024-12-25 12:48:51 +0500

    Code updated

---
+++ src/app/portal/owner/journals/utilities/data.js
@@ -0,0 +1,32 @@
+export const journalTableHeadCells = [
+  {
+    id: 'name',
+    label: 'Name',
+  },
+  {
+    id: 'phone',
+    label: 'Phone',
+  },
+  {
+    id: 'email',
+    label: 'Email',
+  },
+  {
+    id: 'price',
+    label: 'Price',
+  },
+  {
+    id: 'created_at',
+    label: 'Created At',
+  },
+  {
+    id: 'description',
+    label: 'Journal Description',
+  },
+  {
+    id: 'action',
+    label: '',
+  }
+];
+
+export const test = '';
